1795f8068217452197bb49a8dc11b7ec22d521c4
TheWither125 <69727034+TheWither125@users.noreply.github.com>
2020-08-24 20:04:46 +0200

    Update 1.1

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PREFIX = '?'
 
 const client = new Client({ disableEveryone: true})
 
+const queue = new Map()
+
 client.on('ready', () => console.log('Active'))
 
 client.on('message', async message => {
@@ -11,6 +13,7 @@ client.on('message', async message => {
     if(!message.content.startsWith(PREFIX)) return
 
     const args = message.content.substring(PREFIX.length).split(" ")
+    const ServerQueue = queue.get(message.guild.id)
 
     if(message.content.startsWith(`${PREFIX}play`)) {
         const voiceChannel = message.member.voice.channel
@@ -19,27 +22,75 @@ client.on('message', async message => {
         if(!permissions.has('CONNECT')) return message.channel.send("I dont have permissions to connect to the voice channel")
         if(!permissions.has('SPEAK')) return message.channel.send("I dont have permissions to speak in the channel") 
 
-        try {
-            var connection = await voiceChannel.join()
-        } catch (error) {
-            console.log('There was an error connecting to the voice channel: ${error}')
-            return message.channel.send('There was an error connecting to the voice channel: ${error}')
+        const songInfo = await ytdl.getInfo(args[1])
+        const song = {
+            title: songInfo.title,
+            url: songInfo.video_url
         }
 
-        const dispatcher = connection.play(ytdl(args[1]))
+        if(!serverQueue) {
+            const queueConstruct = {
+                textChannel: message.channel,
+                voiceChannel: voiceChannel,
+                connection: null,
+                song: [],
+                volume: 5,
+                playing: true
+            }
+            queue.set(message.guild.id, queueConstruct)
+
+            queueConstruct.songs.push(song)
+        
+            try {
+                var connection = await voiceChannel.join()
+                queueConstruct.connection = connection
+                play(message.guild, queueConsruct.songs[0])
+            } catch (error) {
+                console.log('There was an error connecting to the voice channel: ${error}')
+                queue.delete(message.guild.id)
+                return message.channel.send('There was an error connecting to the voice channel: ${error}')
+           }
+       } else {
+           serverQueue.songs.push(song)
+           return message.channel.send(`**${song.title}** has been added to the queue`)
+       }
+       return undefined
+        
+    } else if(message.content.startsWith(`${PREFIX}stop`)) {
+        if(!message.member.voice.channel) return message.channel.send("You need to be in voice channel to stop the music")
+        if(!serverQueue) return message.channel.send("There is nothing playing")
+        serverQueue.songs = []
+        serverQueue.connection.dispatcher.end()
+        message.channel.send("Music stoped")
+        return undefined
+    } else if (message.content.startsWith(`${PREFIX}skip`)) {
+        if(!message.member.voice.channel) return message.channel.send("You need to be in a voice channel to skip the music")
+        if(!serverQueue) return message.channel.send("There is nothing playing")
+        serverQueue.connection.dispatcher.end()
+        message.channel.send("Music skipped")
+        return undefined
+    }
+})
+
+function play(guild, song) {
+    const ServerQueue = queue.get(guild.id)
+
+    if(!song) {
+        serverQueue.voiceChannel.leave()
+        queue.delete(guild.id)
+        return
+    }
+
+    const dispatcher = serverQueue.connection.play(ytdl(song.url))
         .on('finish', () => {
-            voiceChannel.leave()
+            serverQueue.songs.shift()
+            play(guild, serverQueue.songs[0])
         })
         .on('error', error => {
             console.log(error)
         })
-        dispatcher.setVolumeLogarithmic(5 / 5)
-    } else if(message.content.startsWith(`${PREFIX}stop`)) {
-        if(!message.member.voice.channel) return message.channel.send("You need to be in voice channel to stop the music")
-        message.member.voice.channel.leave()
-        return undefined
-    }
-})
+        dispatcher.setVolumeLogarithmic(serverQueue.volume / 5)
+}
 
 client.login(process.env.token);
 
